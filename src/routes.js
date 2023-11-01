ae71c8426b0a36511f8444fbf74bd2a249b168ed
I585108 <ana.beatriz.stahl@sap.com>
2023-11-01 01:34:18 -0300

    finishing the routes for the application

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,25 @@
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
+            const { search } = req.query
+            const users = database.select('users', search ? {
+                name: search,
+                email: search
+            } : null)
             return res.end(JSON.stringify(users))
         }
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body
             const user = {
@@ -25,6 +30,28 @@ export const routes = [
             database.insert('users', user)
 
             return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('users', id)
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
+            database.update('users', id, {
+                name,
+                email
+            })
+            return res.writeHead(204).end()
         }
     }
 ]
