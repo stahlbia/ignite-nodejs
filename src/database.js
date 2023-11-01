c06cee3006c67c95c33291e6807bc8bd6e4f859c
I585108 <ana.beatriz.stahl@sap.com>
2023-11-01 00:21:07 -0300

    database

---
+++ src/database.js
@@ -0,0 +1,33 @@
+import fs from 'node:fs/promises'
+const databasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
+    #database = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf8').then(data => {
+            this.#database = JSON.parse(data)
+        }).catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
+
+    select(table) {
+        const data = this.#database[table] ?? []
+        this.#persist()
+        return data
+    }
+
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data)
+        } else {
+            this.#database[table] = [data]
+        }
+        return data
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -9,16 +10,18 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     if (method === 'GET' && url === '/users') {
+        const users = database.select('users')
         return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
         const { name, email } = req.body
-        users.push({
-            id: 1,
+        const user = {
+            id: randomUUID(),
             name,
             email,
-        })
+        }
+        database.insert('users', user)
 
         return res.writeHead(201).end()
     }
