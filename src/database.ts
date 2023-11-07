7a995ba8b9dfc8695aff31deb3fdd4754f9120c9
I585108 <ana.beatriz.stahl@sap.com>
2023-11-07 18:43:03 -0300

    squash the commits from the REST API to use the code as base
    REST API: database content
    
    REST API: implementing routes
    
    REST API: adding automated tests
    
    REST API: production config
    
    REST API: removing test.db from the repo
    
    REST API: adding test.db to gitignore
    
    REST API: adding the observations that were made later
    
    REST API: changing error with tsx

---
+++ src/database.ts
@@ -0,0 +1,21 @@
+import createKnex, { Knex } from "knex";
+import { env } from "./env";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env not found.");
+}
+
+export const knexConfig: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+  useNullAsDefault: true,
+};
+
+export const knex = createKnex(knexConfig);
--- src/server.js
+++
@@ -1,7 +0,0 @@
-import http from 'node:http'
-
-const server = http.createServer((req, res) => {
-    return res.end("Batata")
-})
-
-server.listen(3333)
